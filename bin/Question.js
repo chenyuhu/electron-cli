a546902b110ad439ade6e3abb0342abcfa1c90a2
陈锋 <1030304751@qq.com>
2019-12-19 19:59:22 +0800

    :construction: 搭建cli基础模板

---
+++ bin/Question.js
@@ -0,0 +1,61 @@
+module.exports = [
+  {
+    name: 'name',
+    type: 'input',
+    message: 'Please enter a project name',
+    default: 'electronVueTemplate',
+    validate: (val) => {
+      if (!val) {
+        return 'Project name is required'
+      }
+      return true
+    },
+  },
+  {
+    name: 'version',
+    type: 'input',
+    message: 'Please enter the version',
+    default: '0.0.1',
+    validate: (version) => {
+      const rex = /^\d+.\d+.\d+(\w+)?$/
+      if (!rex.test(version)) {
+        return 'Incorrect version format'
+      }
+      return true
+    },
+  },
+  {
+    name: 'author',
+    type: 'input',
+    message: 'Please enter author',
+  },
+  {
+    name: 'description',
+    type: 'input',
+    message: 'What is your description',
+  },
+  {
+    name: 'license',
+    type: 'list',
+    message: 'please choose license',
+    choices: ['MIT', 'ISC', 'null'],
+  },
+  {
+    name: 'jsType',
+    type: 'list',
+    message: 'Use javascript or typescript',
+    choices: ['javascript', 'typescript'],
+  },
+  {
+    name: 'useRouter',
+    type: 'confirm',
+    message: 'Whether to use vue router',
+    default: true,
+  },
+  {
+    name: 'useVuex',
+    type: 'confirm',
+    message: 'Whether to use vuex',
+    default: true,
+  },
+]
